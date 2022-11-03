c199ee5820bb8181e8e31719ce5e5b5a760f78e4
Shindey <shindeymadrid@gmail.com>
2022-11-02 22:31:54 -0300

    sixth commit/Version final TOP con carrusel

--- modal.js
+++ modal.js
@@ -9,3 +9,21 @@ openModal.addEventListener("click", () => {
 closeModal.addEventListener("click", () => {
   modal.close();
 });
+
+const grande = document.querySelector(".grande");
+const punto = document.querySelectorAll(".punto");
+
+punto.forEach((cadaPunto, i) => {
+  punto[i].addEventListener("click", () => {
+    let posicion = i;
+    let operacion = posicion * -25;
+
+    grande.style.transform = `translateX(${operacion}%)`;
+
+    punto.forEach((cadaPunto, i) => {
+      punto[i].classList.remove("activo");
+    });
+
+    punto[i].classList.add("activo");
+  });
+});
